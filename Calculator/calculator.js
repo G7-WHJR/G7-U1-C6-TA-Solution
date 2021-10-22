e082c6ba29a4c78aabf010d0987d56b52d8e88da
unknown <raveen.anand@whitehatjr.com>
2021-10-22 12:59:16 +0530

    v2

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,19 +1,17 @@
-/**Function add(): adds two numbers from the input and displays the answer */
+/**Function add(): adds two numbers and displays the answer */
 function add() {
 
-    //take number 1 from input box "n1" and store it in a variable named "input1"
+    //1. take number 1 from input box "n1" and store it in a variable named "input1"
     var input1=Number(document.getElementById("n1").value);
 
-     //take number 2 from input box "n1" and store it in a variable named "input2"
+    //2. take number 2 from input box "n2" and store it in a variable named "input2"
     var input2=Number(document.getElementById("n2").value);
     
-    //add input 1 and input 2 and store it in a variable named "result"
+    //3. add input 1 and input 2 and store it in a variable named "result"
     var result = input1 + input2;
 
-    console.log(result);
-    // display the result in an HTML element with id "answer" 
+    //4. display the result in an HTML element with id "answer" 
     document.getElementById("answer").innerHTML=result;
 }
-
     
     
